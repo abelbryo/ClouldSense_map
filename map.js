62b53d7e4975a85403e6d41c426094e270600c58
Abel Terefe <abelbryo@gmail.com>
2013-01-31 14:48:33 +0200

    Working map

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-var map;
-function initialize(){
+ var map;
+function init(){
     var mapOptions = {
         center: new google.maps.LatLng(60.18718, 24.81886),
         zoom: 19,
@@ -14,11 +14,9 @@ function initialize(){
 
     var tuasTiles = new google.maps.ImageMapType( {
 
-
         getTileUrl: function (coord, zoom) {
-
                         var proj = map.getProjection(); 
-                        var zoomFactor = Math.pow(2, zoom); 
+                        var zoomFactor = 1 << zoom; // Math.pow(2, zoom); 
 
                         var top = proj.fromPointToLatLng(
                             new google.maps.Point(coord.x * WIDTH / zoomFactor, coord.y * HEIGHT / zoomFactor));
@@ -36,6 +34,7 @@ function initialize(){
                     url += "&VERSION=1.1.1";
                     url += "&REQUEST=GetMap";
                     url += "&LAYERS=" + "cite:tuas_pyramid";
+                    url += "&FORMAT=image/png";
                     url += "&BGCOLOR=0xFFFFFF";
                     url += "&TRANSPARENT=TRUE";
                     url += "&SRS=EPSG:4326";
@@ -50,7 +49,5 @@ function initialize(){
         isPng: true
     });
 
-    //map.overlayMapTypes.push(tuasTiles);
-
-
+    map.overlayMapTypes.push(tuasTiles);
 }
---
+++ sandbox/test.js
@@ -0,0 +1,4 @@
+function init() {
+    var val = document.getElementById("content");
+    val.innerHTML = "Hello World";
+}
