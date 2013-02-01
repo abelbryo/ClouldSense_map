a3a5005d23ec1fdddc873dd27082309ab160da81
Abel Terefe <abelbryo@gmail.com>
2013-02-01 10:05:41 +0200

    Refactoring and settin opacity

--- map.js
+++ map.js
@@ -1,4 +1,48 @@
 var map; // Reference to base google map
+
+var WIDTH = 547; 
+var HEIGHT = 330;
+
+var tuasTiles = new google.maps.ImageMapType( {
+
+    getTileUrl: function (coord, zoom) {
+            var proj = map.getProjection(); 
+            var zoomFactor = 1 << zoom; // 2^zoom
+
+            var top = proj.fromPointToLatLng(
+                    new google.maps.Point(coord.x * WIDTH / zoomFactor, coord.y * HEIGHT / zoomFactor));
+            var bottom = proj.fromPointToLatLng(
+                    new google.maps.Point((coord.x + 1) * WIDTH / zoomFactor, (coord.y + 1) * HEIGHT / zoomFactor));
+
+            var bbox = top.lng()+","+
+                        bottom.lat()+","+
+                        bottom.lng()+","+
+                        top.lat();
+
+            /* WMS request url */
+            var url = "http://localhost:8080/geoserver/cite/wms?";
+            url += "SERVICE=WMS";
+            url += "&VERSION=1.1.1";
+            url += "&REQUEST=GetMap";
+            url += "&LAYERS=" + "cite:tuas_pyramid";
+            url += "&FORMAT=image/png";
+            url += "&BGCOLOR=0xFFFFFF";
+            url += "&TRANSPARENT=TRUE";
+            url += "&SRS=EPSG:4326";
+            url += "&BBOX="+bbox; 
+            url += "&WIDTH="+WIDTH; 
+            url += "&HEIGHT="+HEIGHT;
+
+            return url;
+        }, 
+
+        tileSize: new google.maps.Size(WIDTH, HEIGHT), 
+        isPng: true,
+        name: "TUAS Talo",
+        opacity: 0.8
+
+    });
+
 function init(){
     detectBrowser();  // Different layouts for mobile and desktop
     var mapOptions = {
@@ -6,52 +50,12 @@ function init(){
         zoom: 19,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.TERRAIN],
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.TERRAIN, tuasTiles],
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
         }
     };
 
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
-    var WIDTH = 547; 
-    var HEIGHT = 330;
-
-    var tuasTiles = new google.maps.ImageMapType( {
-
-        getTileUrl: function (coord, zoom) {
-                        var proj = map.getProjection(); 
-                        var zoomFactor = 1 << zoom; // 2^zoom
-
-                        var top = proj.fromPointToLatLng(
-                            new google.maps.Point(coord.x * WIDTH / zoomFactor, coord.y * HEIGHT / zoomFactor));
-                        var bottom = proj.fromPointToLatLng(
-                            new google.maps.Point((coord.x + 1) * WIDTH / zoomFactor, (coord.y + 1) * HEIGHT / zoomFactor));
-
-                        var bbox = top.lng()+","+
-                                   bottom.lat()+","+
-                                   bottom.lng()+","+
-                                   top.lat();
-        
-                    /* WMS request url */
-                    var url = "http://localhost:8080/geoserver/cite/wms?";
-                    url += "SERVICE=WMS";
-                    url += "&VERSION=1.1.1";
-                    url += "&REQUEST=GetMap";
-                    url += "&LAYERS=" + "cite:tuas_pyramid";
-                    url += "&FORMAT=image/png";
-                    url += "&BGCOLOR=0xFFFFFF";
-                    url += "&TRANSPARENT=TRUE";
-                    url += "&SRS=EPSG:4326";
-                    url += "&BBOX="+bbox; 
-                    url += "&WIDTH="+WIDTH; 
-                    url += "&HEIGHT="+HEIGHT;
-
-                    return url;
-        }, 
-
-        tileSize: new google.maps.Size(WIDTH, HEIGHT), 
-        isPng: true
-    });
     map.overlayMapTypes.push(tuasTiles);
 } 
 
